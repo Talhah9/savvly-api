0ae62437630dbded2f342621ea62bbd1823659b1
Talhah9 <71754550+Talhah9@users.noreply.github.com>
2026-03-19 14:36:31 +0100

    Update trainers.js

--- src/routes/trainers.js
+++ src/routes/trainers.js
@@ -4,8 +4,8 @@ const {
   getMe, updateMe, applyTrainer
 } = require('../controllers/trainers.controller')
 
-router.get('/me', authenticate, requireRole('trainer'), getMe)
-router.put('/me', authenticate, requireRole('trainer'), updateMe)
+router.get('/me', authenticate, getMe)
+router.put('/me', authenticate, updateMe)
 router.post('/apply', applyTrainer)
 
 module.exports = router
